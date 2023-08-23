d7b798c6a818af7c9ab42bdade9f978908a52086
hexian000 <hexian000@outlook.com>
2023-08-23 20:30:07 +0800

    conf: fix default conf
    Signed-off-by: hexian000 <hexian000@outlook.com>

--- src/conf.c
+++ src/conf.c
@@ -240,7 +240,7 @@ static struct config conf_default(void)
 		.kcp_mtu = 1400,
 		.kcp_sndwnd = 256,
 		.kcp_rcvwnd = 256,
-		.kcp_nodelay = 2,
+		.kcp_nodelay = 1,
 		.kcp_interval = 50,
 		.kcp_resend = 0,
 		.kcp_nc = 1,
