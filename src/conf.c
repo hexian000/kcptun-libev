0ecc9bc086dfa0924db2c74cc8e5b87037df9bb9
hexian000 <hexian000@outlook.com>
2023-10-22 10:37:33 +0800

    conf: set a conservative mtu value
    Signed-off-by: hexian000 <hexian000@outlook.com>

--- src/conf.c
+++ src/conf.c
@@ -216,7 +216,7 @@ const char *runmode_str(const int mode)
 static struct config conf_default(void)
 {
 	return (struct config){
-		.kcp_mtu = 1492,
+		.kcp_mtu = 1400,
 		.kcp_sndwnd = 256,
 		.kcp_rcvwnd = 256,
 		.kcp_nodelay = 1,
