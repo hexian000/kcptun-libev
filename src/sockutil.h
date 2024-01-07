66fd747ddfd71ee90959349929aa0e13ebac02ba
hexian000 <hexian000@outlook.com>
2024-01-07 19:06:03 +0800

    build: check libev version
    Signed-off-by: hexian000 <hexian000@outlook.com>

--- src/sockutil.h
+++ src/sockutil.h
@@ -40,9 +40,10 @@ bool sa_equals(const struct sockaddr *a, const struct sockaddr *b);
 bool sa_matches(const struct sockaddr *bind, const struct sockaddr *dest);
 int format_sa(const struct sockaddr *sa, char *buf, size_t buf_size);
 
-enum { RESOLVE_TCP = 0x0,
-       RESOLVE_UDP = 0x1,
-       RESOLVE_PASSIVE = 0x2,
+enum {
+	RESOLVE_TCP = 0x0,
+	RESOLVE_UDP = 0x1,
+	RESOLVE_PASSIVE = 0x2,
 };
 bool resolve_addr(union sockaddr_max *sa, const char *s, int flags);
 
