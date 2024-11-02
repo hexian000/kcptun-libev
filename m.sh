d4302836deac4bdcd86dd0028e30914fa73f03dc
He Xian <hexian000@outlook.com>
2024-11-02 10:37:16 +0800

    build: bump cmake to 3.13 for interprocedural optimization
    Signed-off-by: He Xian <hexian000@outlook.com>

--- m.sh
+++ m.sh
@@ -14,10 +14,11 @@ case "$1" in
     # cross compiling, environment vars need to be set
     rm -rf "build" && mkdir "build"
     cmake -G "${GENERATOR}" \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_SYSTEM_NAME="Linux" \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -DCMAKE_FIND_ROOT_PATH="${SYSROOT};${LIBROOT}" \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -S "." -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
     ls -lh "build/bin/kcptun-libev"
@@ -26,8 +27,9 @@ case "$1" in
     # rebuild for strict POSIX compliance
     rm -rf "build" && mkdir "build"
     cmake -G "${GENERATOR}" \
-        -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -DFORCE_POSIX=1 \
         -S "." -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
@@ -37,10 +39,11 @@ case "$1" in
     # rebuild with Linux clang/lld
     rm -rf "build" && mkdir "build"
     cmake -G "${GENERATOR}" \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
         -DCMAKE_C_COMPILER="clang" \
         -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld --rtlib=compiler-rt" \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -S "." -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
     (cd "build/bin" && llvm-objdump -drwS "kcptun-libev" >"kcptun-libev.S")
@@ -51,8 +54,8 @@ case "$1" in
     rm -rf "build" && mkdir "build"
     cmake -G "${GENERATOR}" \
         -DCMAKE_BUILD_TYPE="Release" \
-        -DENABLE_LTO=OFF \
         -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc" \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -S "." -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
     HOST="$(cc -dumpmachine)"
@@ -71,6 +74,7 @@ case "$1" in
         -DCMAKE_SYSTEM_VERSION="${ANDROID_API_LEVEL}" \
         -DCMAKE_ANDROID_ARCH_ABI="${ABI_NAME}" \
         -DCMAKE_FIND_ROOT_PATH="${SYSROOT};${LIBROOT}" \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -DLINK_STATIC_LIBS=ON \
         -S "." -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
@@ -96,10 +100,10 @@ case "$1" in
     # rebuild with clang & sanitizers
     rm -rf "build" && mkdir "build"
     cmake -G "${GENERATOR}" \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -DCMAKE_BUILD_TYPE="Debug" \
         -DCMAKE_C_COMPILER="clang" \
         -DENABLE_SANITIZERS=ON \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -S "." -B "build"
     nice cmake --build "build" --parallel
     ls -lh "build/bin/kcptun-libev"
@@ -108,8 +112,9 @@ case "$1" in
     # rebuild for minimized size
     rm -rf "build" && mkdir "build"
     cmake -G "${GENERATOR}" \
-        -DCMAKE_BUILD_TYPE="MinSizeRel" \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DCMAKE_BUILD_TYPE="MinSizeRel" \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -S "." -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
     ls -lh "build/bin/kcptun-libev"
@@ -118,8 +123,9 @@ case "$1" in
     # rebuild for profiling
     rm -rf "build" && mkdir "build"
     cmake -G "${GENERATOR}" \
-        -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -S "." -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
     (cd "build/bin" && objdump -drwS "kcptun-libev" >"kcptun-libev.S")
@@ -129,8 +135,9 @@ case "$1" in
     # rebuild for release
     rm -rf "build" && mkdir "build"
     cmake -G "${GENERATOR}" \
-        -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -S "." -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
     ls -lh "build/bin/kcptun-libev"
