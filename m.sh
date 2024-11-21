41a7df49284aecc694074ef509b1d72b0ae3d260
He Xian <hexian000@outlook.com>
2024-11-21 19:18:52 +0800

    build: script cleanup
    Signed-off-by: He Xian <hexian000@outlook.com>

--- m.sh
+++ m.sh
@@ -6,62 +6,63 @@ set -ex
 case "$1" in
 "c")
     # clean artifacts
-    rm -rf "build" "compile_commands.json"
+    rm -rf build compile_commands.json
     ;;
 "x")
     # cross compiling, environment vars need to be set
-    rm -rf "build" && mkdir "build"
+    rm -rf build && mkdir -p build && cd build
     cmake \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_SYSROOT="${SYSROOT}" \
         -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -DCMAKE_SKIP_RPATH=ON \
-        -S "." -B "build"
-    cmake --build "build"
-    ls -lh "build/bin/kcptun-libev"
+        ..
+    cmake --build .
+    ls -lh bin/kcptun-libev
     ;;
 "posix")
     # rebuild for strict POSIX compliance
-    rm -rf "build" && mkdir "build"
+    rm -rf build && mkdir -p build && cd build
     cmake \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -DCMAKE_BUILD_TYPE="Release" \
         -DFORCE_POSIX=ON \
-        -S "." -B "build"
-    cp build/compile_commands.json compile_commands.json
-    cmake --build "build"
-    ls -lh "build/bin/kcptun-libev"
+        ..
+    cp compile_commands.json ../
+    cmake --build .
+    ls -lh bin/kcptun-libev
     ;;
 "clang")
     # rebuild with Linux clang/lld
-    rm -rf "build" && mkdir "build"
+    rm -rf build && mkdir -p build && cd build
     cmake \
         -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
         -DCMAKE_C_COMPILER="clang" \
         -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld --rtlib=compiler-rt" \
         -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
-        -S "." -B "build"
-    cmake --build "build"
-    (cd "build/bin" && llvm-objdump -drwS "kcptun-libev" >"kcptun-libev.S")
-    ls -lh "build/bin/kcptun-libev"
+        ..
+    cmake --build .
+    (cd bin && llvm-objdump -drwS kcptun-libev >kcptun-libev.S)
+    ls -lh bin/kcptun-libev
     ;;
 "msys2")
     # rebuild with MSYS 2
-    rm -rf "build" && mkdir "build"
+    rm -rf build && mkdir -p build && cd build
     cmake \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc" \
-        -S "." -B "build"
-    cmake --build "build"
+        -DENABLE_SANITIZERS=OFF \
+        ..
+    cmake --build .
     HOST="$(cc -dumpmachine)"
-    zip -9j "build/kcptun-libev-win32.${HOST}.zip" \
+    zip -9j "kcptun-libev-win32.${HOST}.zip" \
         "/usr/bin/msys-2.0.dll" \
-        "build/bin/kcptun-libev.exe"
-    ls -lh "build/kcptun-libev-win32.${HOST}.zip"
+        "bin/kcptun-libev.exe"
+    ls -lh "kcptun-libev-win32.${HOST}.zip"
     ;;
 "ndk")
     # rebuild with Android NDK
-    rm -rf "build" && mkdir "build"
+    rm -rf build && mkdir -p build && cd build
     cmake \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_SYSTEM_NAME="Android" \
@@ -70,16 +71,14 @@ case "$1" in
         -DCMAKE_ANDROID_ARCH_ABI="${ABI_NAME}" \
         -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -DLINK_STATIC_LIBS=ON \
-        -S "." -B "build"
-    cmake --build "build"
-    ls -lh "build/bin/kcptun-libev"
+        ..
+    cmake --build .
+    ls -lh bin/kcptun-libev
     ;;
 "single")
     # rebuild as a single file
-    rm -rf "build" && mkdir -p "build/src"
-    cmake \
-        -DCMAKE_BUILD_TYPE="Release" \
-        -S "." -B "build"
+    rm -rf build && mkdir -p build/src
+    (cd build && cmake -DCMAKE_BUILD_TYPE="Release" ..)
     find contrib src -name '*.c' | while read -r FILE; do
         echo "#include \"${FILE}\""
     done | gcc -pipe -O3 -s -DNDEBUG -D_GNU_SOURCE -pedantic -Wall -Wextra -std=c11 \
@@ -88,70 +87,71 @@ case "$1" in
         -flto=auto -fno-fat-lto-objects -flto-partition=one \
         -fwhole-program \
         -fPIE -pie \
-        -o "build/bin/kcptun-libev" -xc - -lev -lsodium -lm
-    ls -lh "build/bin/kcptun-libev"
+        -o build/bin/kcptun-libev -xc - -lev -lsodium -lm
+    ls -lh build/bin/kcptun-libev
     ;;
 "san")
     # rebuild with clang & sanitizers
-    rm -rf "build" && mkdir "build"
+    rm -rf build && mkdir -p build && cd build
     cmake \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -DCMAKE_BUILD_TYPE="Debug" \
         -DCMAKE_C_COMPILER="clang" \
         -DENABLE_SANITIZERS=ON \
-        -S "." -B "build"
-    cp build/compile_commands.json compile_commands.json
-    cmake --build "build"
-    ls -lh "build/bin/kcptun-libev"
+        ..
+    cp compile_commands.json ../
+    cmake --build .
+    ls -lh bin/kcptun-libev
     ;;
 "min")
     # rebuild for minimized size
-    rm -rf "build" && mkdir "build"
+    rm -rf build && mkdir -p build && cd build
     cmake \
         -DCMAKE_BUILD_TYPE="MinSizeRel" \
         -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
-        -S "." -B "build"
-    cmake --build "build"
-    ls -lh "build/bin/kcptun-libev"
+        ..
+    cmake --build .
+    ls -lh bin/kcptun-libev
     ;;
 "p")
     # rebuild for profiling
-    rm -rf "build" && mkdir "build"
+    rm -rf build && mkdir -p build && cd build
     cmake \
         -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
         -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
-        -S "." -B "build"
-    cmake --build "build"
-    (cd "build/bin" && objdump -drwS "kcptun-libev" >"kcptun-libev.S")
-    ls -lh "build/bin/kcptun-libev"
+        ..
+    cmake --build .
+    (cd bin && objdump -drwS kcptun-libev >kcptun-libev.S)
+    ls -lh bin/kcptun-libev
     ;;
 "r")
     # rebuild for release
-    rm -rf "build" && mkdir "build"
+    rm -rf build && mkdir -p build && cd build
     cmake \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -DCMAKE_BUILD_TYPE="Release" \
-        -S "." -B "build"
-    cp build/compile_commands.json compile_commands.json
-    cmake --build "build"
-    ls -lh "build/bin/kcptun-libev"
+        ..
+    cp compile_commands.json ../
+    cmake --build .
+    ls -lh bin/kcptun-libev
     ;;
 "d")
     # rebuild for debug
     if command -v clang-format >/dev/null; then
         find src -type f -regex '.*\.[hc]' -exec clang-format -i {} +
     fi
-    rm -rf "build" && mkdir -p "build"
+    rm -rf build && mkdir -p build && cd build
     cmake \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -DCMAKE_BUILD_TYPE="Debug" \
-        -S . -B "build"
-    cp build/compile_commands.json compile_commands.json
-    cmake --build "build"
-    ls -lh "build/bin/kcptun-libev"
+        ..
+    cp compile_commands.json ../
+    cmake --build .
+    ls -lh bin/kcptun-libev
     ;;
 *)
-    cmake --build "build"
-    ls -lh "build/bin/kcptun-libev"
+    cd build
+    cmake --build .
+    ls -lh bin/kcptun-libev
     ;;
 esac
